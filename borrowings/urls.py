0404d11d1e911fc98acf547354e5b625fe82bf70
NazarKravchenk0 <113011986+NazarKravchenk0@users.noreply.github.com>
2026-04-07 22:53:16 +0300

    Delete borrowings directory

--- borrowings/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from borrowings.views import BorrowingViewSet
-
-
-router = DefaultRouter()
-router.register("", BorrowingViewSet, basename="borrowings")
-
-urlpatterns = router.urls
