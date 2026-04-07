64d3c8a6604f9f6530dfb60b4b1e1ef5e150149a
NazarKravchenk0 <113011986+NazarKravchenk0@users.noreply.github.com>
2026-04-07 22:53:42 +0300

    Delete books directory

--- books/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from books.views import BookViewSet
-
-
-router = DefaultRouter()
-router.register("", BookViewSet, basename="books")
-
-urlpatterns = router.urls
