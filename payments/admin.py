22a3b1ceda57e78107220b48a7f012979692575d
NazarKravchenk0 <113011986+NazarKravchenk0@users.noreply.github.com>
2026-04-07 22:53:25 +0300

    Delete payments directory

--- payments/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from payments.models import Payment
-
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ("id", "borrowing", "type", "status", "money_to_pay")
-    list_filter = ("status", "type")
