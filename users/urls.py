a7e325a6126b1b25041f875d5632fde75e0829b6
NazarKravchenk0 <113011986+NazarKravchenk0@users.noreply.github.com>
2026-04-07 22:53:33 +0300

    Delete users directory

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from users.views import ManageUserView, UserCreateView
-
-
-urlpatterns = [
-    path("", UserCreateView.as_view(), name="create-user"),
-    path("me/", ManageUserView.as_view(), name="manage-user"),
-]
